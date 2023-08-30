0e280d3e381ea1524bec8ee73446db715739a087
Adi Darmawan <adi.darmawan1711@gmail.com>
2023-08-30 14:21:42 +0700

    Update var.sh

--- .github/workflows/var.sh
+++ .github/workflows/var.sh
@@ -3,7 +3,7 @@
 LINK_TO_APK_FILE_IN_GCP="TEST FILE"
 
 echo ${LINK_TO_APK_FILE_IN_GCP}
-export LINK_TO_APK_FILE_IN_GCP
+export ${LINK_TO_APK_FILE_IN_GCP}
 testing_echo() {
     echo ${LINK_TO_APK_FILE_IN_GCP}
 }
